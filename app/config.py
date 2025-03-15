d7e63255a3056e11e7ed3d88fbac75e6850aedc9
obula reddy selena <obulguda@gmail.com>
2025-03-15 11:43:01 +0530

    updated config file

--- app/config.py
+++ app/config.py
@@ -1,14 +1,15 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
-    database_hostname:str
-    database_port:str
-    database_password:str
-    database_username:str
-    database_name:str
-    secret_key:str
-    algorithm:str
-    access_token_expire_minutes:int
+    database_hostname: str = os.getenv("DATABASE_HOSTNAME")
+    database_port: str = os.getenv("DATABASE_PORT")
+    database_password: str = os.getenv("DATABASE_PASSWORD")
+    database_username: str = os.getenv("DATABASE_USERNAME")
+    database_name: str = os.getenv("DATABASE_NAME")
+    secret_key: str = os.getenv("SECRET_KEY")
+    algorithm: str = os.getenv("ALGORITHM")
+    access_token_expire_minutes: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 
     class Config:
         env_file = ".env"
